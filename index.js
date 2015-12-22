659882e3346c766d9aec0ed383d6e2a89c527d93
Aria Stewart <aredridel@dinhe.net>
2015-12-22 17:44:57 -0500

    Fix where

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 "use strict";
 const confit = require('confit');
 const P = require('bluebird');
+const knex = require('knex');
 
 P.promisifyAll(confit(__dirname)).createAsync().then(function(config) {
-  const db = require('knex')({
+  const db = knex({
     client: 'pg',
     connection: config.get('connection'),
     pool: config.get('pool')
   });
 
-  return db('users').where('created > now() - interval \'1 hour\'').then(console.log)
+  return db('users').where('created', '>', knex.raw('now() - interval \'1 hour\'')).then(console.log)
 }).catch(function(err) {
   console.warn(err.stack);
   process.exit(1);
