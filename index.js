7c2f2734fdd4edafd4bdcbbe260b8fbb783b587f
Aria Stewart <aredridel@dinhe.net>
2015-12-22 16:00:56 -0500

    Use confit for config loading

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 "use strict";
-const jrk = require('@npmcorp/jrk');
-const config = require('@npm/enterprise-configurator').Config();
+const confit = require('confit');
+
+confit(__dirname).create(function(err, config) {
+  const db = require('knex')({
+    client: 'pg',
+    connection: config.get('connection'),
+    pool: config.get('pool')
+  });
+
+  db('users').where('created > now() - interval \'1 hour\'').then(console.log)
+})
+
