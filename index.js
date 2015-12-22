c76b6f4a68b821616a2c0137c39a73ebcd24c77d
Aria Stewart <aredridel@dinhe.net>
2015-12-22 16:34:06 -0500

    Promisify the whole lifecycle

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 "use strict";
 const confit = require('confit');
+const P = require('bluebird');
 
-confit(__dirname).create(function(err, config) {
+P.promisifyAll(confit(__dirname)).createAsync().then(function(config) {
   const db = require('knex')({
     client: 'pg',
     connection: config.get('connection'),
     pool: config.get('pool')
   });
 
-  db('users').where('created > now() - interval \'1 hour\'').then(console.log)
+  return db('users').where('created > now() - interval \'1 hour\'').then(console.log)
+}).catch(function(err) {
+  console.warn(err.stack);
+  process.exit(1);
 })
 
